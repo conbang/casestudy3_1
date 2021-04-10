b8d25c0bfd6110bf0b9074c46b85c3506a8dfdc4
conbang <tuyen.tcd1@gmail.com>
2021-04-10 17:43:59 +0700

    show product

---
+++ src/main/java/model/Login.java
@@ -0,0 +1,6 @@
+package service;
+
+public interface Login {
+    String query = "Select name from customer where email=? and password=?";
+    boolean login(String email,String psw);
+}
---
+++ src/main/java/model/LoginService.java
@@ -0,0 +1,37 @@
+package service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginService implements Login {
+    @Override
+    public boolean login(String email, String psw) {
+        Connection connection = DatabaseConnection.getConnection();
+        if (connection != null) {
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, email);
+                preparedStatement.setString(2, psw);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    try {
+                        connection.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
