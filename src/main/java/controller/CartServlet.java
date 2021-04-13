bae4c3a0a93ec32eaed3b8a0fcbe614a2af0613d
conbang <tuyen.tcd1@gmail.com>
2021-04-13 08:55:39 +0700

    fix add cart

--- src/main/java/controller/CartServlet.java
+++ src/main/java/controller/CartServlet.java
@@ -10,17 +10,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(name = "CartServlet", value = "/cart")
 public class CartServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int productId = Integer.parseInt(req.getParameter("id"));
+        int quantity = Integer.parseInt(req.getParameter("quantity"));
+        Map<Integer,Integer> listproduct = null;
         HttpSession session = req.getSession(false);
         User user = (User) session.getAttribute("name");
-        int id = user.getUserId();
-        Cart cart = new Cart();
-        cart.addNewProduct(id,productId,1);
+        if (user == null) {
+            listproduct = new HashMap<>();
+            listproduct.put(productId,quantity);
+        }else{
+            if (listproduct != null) {
+
+
+            }else{
+                int id = user.getUserId();
+                Cart cart = new Cart();
+                cart.addNewProduct(id,productId,1);
+            }
+
+        }
     }
 
     @Override
